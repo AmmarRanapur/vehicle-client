b870fb55dccb73f84e3ec710f15fa5c85ffa0796
GLOBAL\ranapura <Ammar.Ranapurwala@cdk.com>
2023-03-06 14:41:53 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Home';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import CreateVehicle from './CreateVehicle';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateVehicle from "./CreateVehicle";
 
 function App() {
   const router = createBrowserRouter([
@@ -16,18 +11,22 @@ function App() {
       element: <Home />,
     },
     {
-      path:"/Add",
-      element:<CreateVehicle />
-    }
+      path: "/Add",
+      element: <CreateVehicle />,
+    },
   ]);
   return (
     <div className="App">
-      <header className="App-header" >
-        <img alt='Logo' src="https://www.cdkglobal.com/sites/cdk4/themes/custom/cdk/logo.svg" height={'30px'}/>
+      <header className="App-header">
+        <img
+          alt="Logo"
+          src="https://www.cdkglobal.com/sites/cdk4/themes/custom/cdk/logo.svg"
+          height={"30px"}
+        />
       </header>
       <RouterProvider router={router} />
     </div>
   );
 }
- 
+
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,66 +1,89 @@
-import './App.css';
-import axios from 'axios';
-import {useState,useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {useNavigate} from "react-router-dom";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
-function Home(props){
+function Home(props) {
   const navigate = useNavigate();
-  const [vehicleList,setVehicleList] = useState([]);
-  useEffect(()=>{
-    getFromApi()
-  },[])
+  const [vehicleList, setVehicleList] = useState([]);
+
+  useEffect(() => {
+    getFromApi();
+  }, []);
+
   const getFromApi = () => {
-    axios.get('https://localhost:7288/vehicle').then((response)=>{
-      setVehicleList(response.data.filter((ele)=>ele.IsActive));
-      console.log(vehicleList);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
+    axios
+      .get("https://localhost:7288/vehicle")
+      .then((response) => {
+        setVehicleList(response.data.filter((ele) => ele.IsActive));
+        console.log(vehicleList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
   function removeVehicle(id) {
-    axios.delete('https://localhost:7288/vehicle/'+id).then((response)=>{
+    axios
+      .delete("https://localhost:7288/vehicle/" + id)
+      .then((response) => {
         console.log(response);
         getFromApi();
-    }).catch((error)=>{
-      console.log(error);
-    })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-    return (
-        <div className='main-div'>
-        <div className="btnDiv">
-          <button class="btn addBtn" onClick={()=>navigate("/Add")}>ADD</button>
-        </div>
-      <table class="table">
-    <thead class="table-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">VIN</th>
-      <th scope="col">Year</th>
-      <th scope="col">Make</th>
-      <th scope="col">Model</th>
-      <th scope="col">Info</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-  {vehicleList.length>0 && vehicleList?.map((item,ind)=><tr>
-    <th scope="row">{ind+1}</th>
-      <td>{item.VIN}</td>
-      <td>{item.Year}</td>
-      <td>{item.Make}</td>
-      <td>{item.Model}</td>
-      <td>{item.Description}</td>
-      <td>
-        <button disabled class="btn btn-primary">Edit</button>
-        <button class="btn btn-danger" style={{marginLeft:'5px'}} onClick={(event)=>{
-          removeVehicle(item.Id);
-        }}> Remove</button>
-      </td>
-  </tr>)}
-  </tbody>
-</table>
+  return (
+    <div className="main-div">
+      <div className="btnDiv">
+        <button class="btn addBtn" onClick={() => navigate("/Add")}>
+          ADD
+        </button>
       </div>
-    );
+      <table class="table">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">VIN</th>
+            <th scope="col">Year</th>
+            <th scope="col">Make</th>
+            <th scope="col">Model</th>
+            <th scope="col">Info</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vehicleList.length > 0 &&
+            vehicleList?.map((item, ind) => (
+              <tr>
+                <th scope="row">{ind + 1}</th>
+                <td>{item.VIN}</td>
+                <td>{item.Year}</td>
+                <td>{item.Make}</td>
+                <td>{item.Model}</td>
+                <td>{item.Description}</td>
+                <td>
+                  <button disabled class="btn btn-primary">
+                    Edit
+                  </button>
+                  <button
+                    class="btn btn-danger"
+                    style={{ marginLeft: "5px" }}
+                    onClick={(event) => {
+                      removeVehicle(item.Id);
+                    }}
+                  >
+                    {" "}
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default Home;
